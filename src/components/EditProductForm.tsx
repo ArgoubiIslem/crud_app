a4a7b0c652ab96e1c3f346b2376e8346ee27d44c
unknown <argoubiislem5@gmail.com>
2023-08-14 18:49:14 +0200

    first commit

--- src/components/EditProductForm.tsx
+++ src/components/EditProductForm.tsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonModal,
+} from "@ionic/react";
+import "./EditProductForm.css"; 
+
 export interface Produit {
   id: number;
   nom: string;
   prix: number;
   quantite: number;
 }
+
 interface EditProductFormProps {
   isOpen: boolean;
   onClose: () => void;
@@ -20,7 +35,7 @@ const EditProductForm: React.FC<EditProductFormProps> = ({
 }) => {
   const [editedProduct, setEditedProduct] = useState<Produit>(product);
 
-  const handleFieldChange = (field: string, value: any) => {
+  const handleFieldChange = (field: keyof Produit, value: any) => {
     setEditedProduct((prevProduct) => ({
       ...prevProduct,
       [field]: value,
@@ -32,31 +47,36 @@ const EditProductForm: React.FC<EditProductFormProps> = ({
     onClose();
   };
 
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <h2>Edit Product</h2>
-      <input
-        type="text"
-        value={editedProduct.nom}
-        onChange={(e) => handleFieldChange("nom", e.target.value)}
-      />
-      <input
-        type="number"
-        value={editedProduct.prix}
-        onChange={(e) => handleFieldChange("prix", Number(e.target.value))}
-      />
-      <input
-        type="number"
-        value={editedProduct.quantite}
-        onChange={(e) => handleFieldChange("quantite", Number(e.target.value))}
-      />
-      <button onClick={handleUpdate}>Update</button>
-      <button onClick={onClose}>Cancel</button>
-    </div>
+    <IonModal isOpen={isOpen}>
+    <div className="container">
+    <h2>Modifier le produit</h2>
+    <IonLabel >Nom</IonLabel>
+    <input
+      type="text"
+      value={editedProduct.nom}
+      onChange={(e) => handleFieldChange("nom", e.target.value)}
+    />
+    <IonLabel >Prix</IonLabel>
+    <input
+      type="number"
+      value={editedProduct.prix}
+      onChange={(e) => handleFieldChange("prix", Number(e.target.value))}
+    />
+    <IonLabel >Quantité</IonLabel>
+    <input
+      type="number"
+      value={editedProduct.quantite}
+      onChange={(e) => handleFieldChange("quantite", Number(e.target.value))}
+    />
+    <IonButton expand="full" onClick={handleUpdate}>
+  Modifier
+</IonButton>
+<IonButton expand="full" onClick={onClose}>
+  Annuler
+</IonButton>
+  </div>
+    </IonModal>
   );
 };
 
